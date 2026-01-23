0f13ca7cc40dd2bf766225b8c4967c8b79570d3a
Kingsley Eze <firebox@ikingsley.com>
2026-01-23 17:09:50 +0100

    feat(kotlin-sdk): add FlagKit exception class

---
+++ src/main/kotlin/dev/flagkit/FlagKitException.kt
@@ -0,0 +1,27 @@
+package dev.flagkit
+
+/**
+ * Exception for FlagKit SDK errors.
+ */
+class FlagKitException(
+    val errorCode: ErrorCode,
+    override val message: String,
+    override val cause: Throwable? = null
+) : Exception("[$${errorCode.code}] $message", cause) {
+
+    val isRecoverable: Boolean
+        get() = errorCode.isRecoverable
+
+    companion object {
+        fun initError(message: String) = FlagKitException(ErrorCode.INIT_FAILED, message)
+
+        fun authError(code: ErrorCode, message: String) = FlagKitException(code, message)
+
+        fun networkError(message: String, cause: Throwable? = null) =
+            FlagKitException(ErrorCode.NETWORK_ERROR, message, cause)
+
+        fun evalError(code: ErrorCode, message: String) = FlagKitException(code, message)
+
+        fun configError(code: ErrorCode, message: String) = FlagKitException(code, message)
+    }
+}
